cea61b4c958a3a95850b7461ec78ae9887fb460f
apoorvsharma57362 <92596739+apoorvsharma57362@users.noreply.github.com>
2024-05-25 14:14:19 +0530

    Add files via upload

---
+++ Tempearture.js
@@ -0,0 +1,27 @@
+const prompt = require("prompt-sync")();
+let temperature = prompt("Enter temperature in Celcius: ");
+
+if(temperature < 0)
+{
+    console.log("Its freezing outside");
+}
+
+else if(temperature >= 0 && temperature <= 10)
+{
+    console.log("Its cold outside");
+}
+
+else if(temperature >= 11 && temperature <= 20)
+{
+    console.log("Its a pleasant day");
+}
+
+else if(temperature >= 21 && temperature <= 30)
+{
+    console.log("Its a warm day");
+}
+
+else
+{
+    console.log("Its hot outside");
+}
